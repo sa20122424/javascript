39fadf4a365c9f733e5e71047ca8b104f7e8bba8
sa20122424 <saloxiddin201224@gmail.com>
2024-07-13 14:45:27 +0500

    js

---
+++ main.js
@@ -0,0 +1,99 @@
+let Ferst_name = prompt("ведите име" )
+console.log(Ferst_name);
+alert("ваше имя "+Ferst_name) 
+
+
+const Ferst_familiya = prompt("ведите свою фамилию" )
+console.log(Ferst_familiya);
+alert("ваше фамилия "+Ferst_familiya) 
+
+    const yosh = prompt(" сколько вам лет")
+    console.log(yosh);
+    alert('вам '+yosh)
+
+    const locate = prompt("где вы живете")
+    console.log(locate);
+    alert('вы живете '+locate)
+
+    const player = prompt("какой ваш любимый предмер")
+    console.log(player);
+    alert('ваш любимый предмет '+player)
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
